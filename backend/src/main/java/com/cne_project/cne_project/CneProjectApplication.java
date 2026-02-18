b62c3e6375fb9e1e6d1ba283d539ad1356d33b69
Juangmz7 <juanjuangmo@gmail.com>
2026-02-18 17:13:32 +0100

    Initial commit

---
+++ backend/src/main/java/com/cne_project/cne_project/CneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cne_project.cne_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CneProjectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/cne_project/cne_project/CneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cne_project.cne_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
