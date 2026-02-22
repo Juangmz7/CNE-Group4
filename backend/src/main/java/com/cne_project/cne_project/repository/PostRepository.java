64df61cfb6ff83c8deabfb205792a8c09214feee
~julia <sapero@icosahedr.online>
2026-02-22 20:49:44 +0100

    empty repositories

---
+++ backend/src/main/java/com/cne_project/cne_project/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.cne_project.cne_project.repository;
+
+import com.cne_project.cne_project.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
