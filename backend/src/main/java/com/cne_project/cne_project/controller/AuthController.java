8305a17ad87205df616e875c183ac87366d592a8
Juangmz7 <juanjuangmo@gmail.com>
2026-02-22 23:21:39 +0100

    Added allArgsConstructor annotation

--- backend/src/main/java/com/cne_project/cne_project/controller/AuthController.java
+++ backend/src/main/java/com/cne_project/cne_project/controller/AuthController.java
@@ -4,19 +4,17 @@ import com.cne_project.cne_project.model.dto.auth.LoginRequestDTO;
 import com.cne_project.cne_project.model.dto.auth.LoginResponseDTO;
 import com.cne_project.cne_project.service.AuthService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @RequestMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@Valid LoginRequestDTO request) {
