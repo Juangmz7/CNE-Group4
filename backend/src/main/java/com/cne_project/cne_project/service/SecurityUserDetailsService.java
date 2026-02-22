758b026a88ad14e4027f1d38c5eb95b0d59226a3
Juangmz7 <juanjuangmo@gmail.com>
2026-02-22 21:47:19 +0100

    Fix: Added service annotation

--- backend/src/main/java/com/cne_project/cne_project/service/SecurityUserDetailsService.java
+++ backend/src/main/java/com/cne_project/cne_project/service/SecurityUserDetailsService.java
@@ -3,7 +3,9 @@ package com.cne_project.cne_project.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SecurityUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
