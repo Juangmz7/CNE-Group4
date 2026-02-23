7548ab5485ee009ae157634d2377e49511742290
Juangmz7 <juanjuangmo@gmail.com>
2026-02-23 12:50:10 +0100

    Removed refreshToken field

--- backend/src/main/java/com/cne_project/cne_project/model/dto/auth/LoginResponseDTO.java
+++ backend/src/main/java/com/cne_project/cne_project/model/dto/auth/LoginResponseDTO.java
@@ -3,7 +3,6 @@ package com.cne_project.cne_project.model.dto.auth;
 public record LoginResponseDTO(
         String username,
         String password,
-        String accessToken,
-        String refreshToken
+        String accessToken
 ) {
 }
