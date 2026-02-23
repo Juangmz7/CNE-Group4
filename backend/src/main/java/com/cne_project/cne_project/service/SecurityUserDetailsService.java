6bfd83ae9cc45140ca3b9cc47d0398588338a87b
Juangmz7 <juanjuangmo@gmail.com>
2026-02-23 13:24:22 +0100

    Implemented user fetch from db

--- backend/src/main/java/com/cne_project/cne_project/service/SecurityUserDetailsService.java
+++ backend/src/main/java/com/cne_project/cne_project/service/SecurityUserDetailsService.java
@@ -1,15 +1,28 @@
 package com.cne_project.cne_project.service;
 
+import com.cne_project.cne_project.model.entity.User;
+import com.cne_project.cne_project.repository.UserRepository;
+import com.cne_project.cne_project.utils.SecurityUser;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class SecurityUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public SecurityUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO: This class is the handler of spring for login automaticallyu
-        return null;
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new SecurityUser(user);
     }
 }
