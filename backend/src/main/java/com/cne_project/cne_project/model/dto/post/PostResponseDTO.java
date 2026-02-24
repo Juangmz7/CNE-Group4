83fb9a9957306f55dfde835be681b1ebaa720af1
Juangmz7 <juanjuangmo@gmail.com>
2026-02-24 21:46:22 +0100

    Added replyToId field

--- backend/src/main/java/com/cne_project/cne_project/model/dto/post/PostResponseDTO.java
+++ backend/src/main/java/com/cne_project/cne_project/model/dto/post/PostResponseDTO.java
@@ -1,7 +1,14 @@
 package com.cne_project.cne_project.model.dto.post;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * @param replyToId  If the post is a reply, it's included otherwise no
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record PostResponseDTO(
         String id,
+        String replyToId,
         String authorId,
         String authorName,
         String content,
