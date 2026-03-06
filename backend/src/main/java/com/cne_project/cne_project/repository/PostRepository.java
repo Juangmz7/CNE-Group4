19a264efef54a82e109de7b51bbcf2eb4aa0db30
Juangmz7 <juanjuangmo@gmail.com>
2026-03-06 16:43:58 +0100

    Added query for return foreign posts

--- backend/src/main/java/com/cne_project/cne_project/repository/PostRepository.java
+++ backend/src/main/java/com/cne_project/cne_project/repository/PostRepository.java
@@ -2,6 +2,13 @@ package com.cne_project.cne_project.repository;
 
 import com.cne_project.cne_project.model.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, String> {
+
+    @Query("SELECT p FROM Post p JOIN FETCH p.owner WHERE p.owner.id != :currentUserId")
+    List<Post> findPostsNotOwnedBy(@Param("currentUserId") String currentUserId);
 }
