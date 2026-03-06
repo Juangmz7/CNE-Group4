300f63fb8bd6d198b4570f6dce6355d1ff0e2860
Juangmz7 <juanjuangmo@gmail.com>
2026-03-06 15:49:07 +0100

    Revert "Implemented methods for manage karma"
    This reverts commit c197ebf271bfcb63dd210f950ec33f5f5c4c94ea.

--- backend/src/main/java/com/cne_project/cne_project/model/entity/UserKarma.java
+++ backend/src/main/java/com/cne_project/cne_project/model/entity/UserKarma.java
@@ -1,13 +1,9 @@
 package com.cne_project.cne_project.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
 @Table(name = "user_karma")
-@NoArgsConstructor
 public class UserKarma {
 
     @Id
@@ -17,12 +13,6 @@ public class UserKarma {
     @Column(nullable = false)
     private Integer karma;
 
-    public UserKarma(String id) {
-        this.id = id;
-        this.karma = 0;
-    }
-
-    public void increaseKarma() { this.karma++; }
-
-    public void decreaseKarma() { this.karma--; }
+    public String getId() { return id; }
+    public Integer getKarma() { return karma; }
 }
