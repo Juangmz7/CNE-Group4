31324ae95e10e0355c748c9a5e9523a9b495138d
Juangmz7 <juanjuangmo@gmail.com>
2026-03-06 15:48:13 +0100

    Revert "Added userkarma storage at user registration"
    This reverts commit 4a86992044f7105c1201e1c598f2b5848744da59.

--- backend/src/main/java/com/cne_project/cne_project/service/AuthService.java
+++ backend/src/main/java/com/cne_project/cne_project/service/AuthService.java
@@ -5,8 +5,6 @@ import com.cne_project.cne_project.model.dto.auth.LoginRequestDTO;
 import com.cne_project.cne_project.model.dto.auth.LoginResponseDTO;
 import com.cne_project.cne_project.model.dto.auth.RegisterRequestDTO;
 import com.cne_project.cne_project.model.entity.User;
-import com.cne_project.cne_project.model.entity.UserKarma;
-import com.cne_project.cne_project.repository.UserKarmaRepository;
 import com.cne_project.cne_project.repository.UserRepository;
 import com.cne_project.cne_project.utils.TokenPayload;
 import jakarta.persistence.EntityNotFoundException;
@@ -33,7 +31,6 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
-    private final UserKarmaRepository userKarmaRepository;
 
     @Transactional(readOnly = true)
     public LoginResponseDTO login (LoginRequestDTO request) {
@@ -86,9 +83,6 @@ public class AuthService {
                 .build();
 
         User savedUser = userRepository.save(user);
-
-        UserKarma userKarma = new UserKarma(savedUser.getId());
-        userKarmaRepository.save(userKarma);
 
         log.debug("User registered successfully with id: {}", user.getId());
     }
