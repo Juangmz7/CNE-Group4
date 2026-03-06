c197ebf271bfcb63dd210f950ec33f5f5c4c94ea
Juangmz7 <juanjuangmo@gmail.com>
2026-03-06 15:30:46 +0100

    Implemented methods for manage karma

--- backend/src/main/java/com/cne_project/cne_project/model/entity/UserKarma.java
+++ backend/src/main/java/com/cne_project/cne_project/model/entity/UserKarma.java
@@ -1,9 +1,13 @@
 package com.cne_project.cne_project.model.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "user_karma")
+@NoArgsConstructor
 public class UserKarma {
 
     @Id
@@ -13,6 +17,12 @@ public class UserKarma {
     @Column(nullable = false)
     private Integer karma;
 
-    public String getId() { return id; }
-    public Integer getKarma() { return karma; }
+    public UserKarma(String id) {
+        this.id = id;
+        this.karma = 0;
+    }
+
+    public void increaseKarma() { this.karma++; }
+
+    public void decreaseKarma() { this.karma--; }
 }
