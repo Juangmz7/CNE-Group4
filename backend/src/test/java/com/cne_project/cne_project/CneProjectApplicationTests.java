2defe16cd3782406ec5fc5b47ee95b8d885225c6
Juangmz7 <juanjuangmo@gmail.com>
2026-04-21 14:41:53 +0200

    Added tests containers for db

--- backend/src/test/java/com/cne_project/cne_project/CneProjectApplicationTests.java
+++ backend/src/test/java/com/cne_project/cne_project/CneProjectApplicationTests.java
@@ -1,9 +1,12 @@
 package com.cne_project.cne_project;
 
+import com.cne_project.cne_project.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 class CneProjectApplicationTests {
 
     @Test
---
+++ backend/src/test/java/com/cne_project/cne_project/config/TestContainersConfig.java
@@ -0,0 +1,16 @@
+package com.cne_project.cne_project.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class TestContainersConfig {
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15-alpine");
+    }
+}
