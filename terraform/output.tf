8acf4d83883b1c440e966a1f4c47ba845b7a4220
Juangmz7 <juanjuangmo@gmail.com>
2026-05-04 17:17:27 +0200

    Add hostnames and urls outputs to ensure a guided deploy

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,18 @@ output "frontend_webapp_url" {
   value       = "https://${azurerm_linux_web_app.frontend_app.default_hostname}"
   description = "Frontend web app URL"
 }
+
+output "frontend_webapp_hostname" {
+  value       = azurerm_linux_web_app.frontend_app.default_hostname
+  description = "Frontend web app hostname"
+}
+
+output "backend_function_app_url" {
+  value       = "https://${azurerm_linux_function_app.spring_func.default_hostname}.azurewebsites.net/api/AzureWebAdapter/api"
+  description = "Backend function app URL (API)"
+}
+
+output "backend_function_app_hostname" {
+  value       = azurerm_linux_function_app.spring_func.default_hostname
+  description = "Backend function app hostname"
+}
