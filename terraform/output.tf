38590bde96b3ef31964afdec89973c2aee26d1cf
Juangmz7 <juanjuangmo@gmail.com>
2026-05-04 17:27:42 +0200

    Fix: Output variables change from hostname to name

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,17 +8,17 @@ output "frontend_webapp_url" {
   description = "Frontend web app URL"
 }
 
-output "frontend_webapp_hostname" {
-  value       = azurerm_linux_web_app.frontend_app.default_hostname
-  description = "Frontend web app hostname"
+output "frontend_webapp_name" {
+  value       = azurerm_linux_web_app.frontend_app.name
+  description = "Frontend web app name"
 }
 
 output "backend_function_app_url" {
-  value       = "https://${azurerm_linux_function_app.spring_func.default_hostname}.azurewebsites.net/api/AzureWebAdapter/api"
+  value       = "https://${azurerm_linux_function_app.spring_func.default_hostname}/api/AzureWebAdapter/api"
   description = "Backend function app URL (API)"
 }
 
-output "backend_function_app_hostname" {
-  value       = azurerm_linux_function_app.spring_func.default_hostname
-  description = "Backend function app hostname"
+output "backend_function_app_name" {
+  value       = azurerm_linux_function_app.spring_func.name
+  description = "Backend function app name"
 }
