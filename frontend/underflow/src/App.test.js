b1bc6bda3b39a5639522817c8f4bdb3137f1a75a
~julia <sapero@icosahedr.online>
2026-05-04 11:48:59 +0200

    dummy test

--- frontend/underflow/src/App.test.js
+++ frontend/underflow/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('unauthenticated home page redirects to sign in', () => {
+    render(<App />);
+    expect(screen.getByText(/sign in/i)).toBeInTheDocument();
 });
